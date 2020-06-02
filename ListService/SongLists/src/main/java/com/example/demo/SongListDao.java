f5e4a4a79f85c96702d433e35658e98cd8ce69ca
metalbored <62143345+metalbored@users.noreply.github.com>
2020-06-02 12:56:51 +0300

    Delete SongListDao.java

--- ListService/SongLists/src/main/java/com/example/demo/SongListDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface SongListDao extends ReactiveMongoRepository<ListEntity, String> {
-
-
-}
