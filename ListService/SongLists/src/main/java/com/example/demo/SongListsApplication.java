079802e36849a05522f615e0fcbffde286524d52
Eden <eg7eg7@gmail.com>
2020-07-03 02:37:06 +0300

    business logic

--- ListService/SongLists/src/main/java/com/example/demo/SongListsApplication.java
+++ ListService/SongLists/src/main/java/com/example/demo/SongListsApplication.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
 
 @SpringBootApplication
-@EnableSwagger2WebFlux
+//@EnableSwagger2WebFlux
 public class SongListsApplication {
 
 	public static void main(String[] args) {
